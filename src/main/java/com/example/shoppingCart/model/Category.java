42bb23dd186512fd5def97fdb6116892004ff640
Dinithi2001 <dinithimadushika2001@gmail.com>
2025-01-26 12:44:24 -0800

    create image,category,product entities

---
+++ src/main/java/com/example/shoppingCart/model/Category.java
@@ -0,0 +1,24 @@
+package com.example.shoppingCart.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+
+}
